b7d879b9cc1f1d60ecfdd0ac1eb3940a0f4c9f20
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-03-29 20:09:30 +0300

    fixes for issue #6 about elastic logs

--- decorators/basic.js
+++ decorators/basic.js
@@ -6,19 +6,23 @@ const basicDecorator = (fn, isAdmin = false) => async (req, res) => {
   // start timer
   req.start = process.hrtime();
 
+  // Logging method
+  const elasticLog = (responseMethod, data) => {
+    const response = responseMethod(data);
+
+    if (res.exception) {
+      indexException(req, res, data);
+    }
+
+    indexEvent(req, res, data);
+    return response;
+  };
+
   // Inject elastisearch indexer into json method
   const resJsonOrig = res.json;
   const resSendOrig = res.send;
-  res.json = (obj) => {
-    const response = resJsonOrig(obj);
-    indexEvent(req, res);
-    return response;
-  };
-  res.send = (obj) => {
-    const response = resSendOrig(obj);
-    indexEvent(req, res);
-    return response;
-  };
+  res.json = data => elasticLog(resJsonOrig, data);
+  res.send = data => elasticLog(resSendOrig, data);
 
   try {
     const { headers } = req;
@@ -32,15 +36,10 @@ const basicDecorator = (fn, isAdmin = false) => async (req, res) => {
     await fn(req, res);
   } catch (e) {
     console.log(e);
-
-    // Here indexing exceptions only
-    if ([500, 502].includes(e.status)) {
-      indexException(req, e);
-    }
-
+    res.exception = e;
     res.status(typeof e.status === 'number' ? e.status : 500).json({
       message: e.message,
-      code: e.code ? e.code : undefined// Can contain useful textual codes
+      ...(e.code ? { code: e.code } : {})// Can contain useful textual codes
     });
   }
 };
--- helpers/elasticsearch/handlers.js
+++ helpers/elasticsearch/handlers.js
@@ -1,37 +1,106 @@
+const { errors } = require('@elastic/elasticsearch');
+
 // Parse caller IP from the request
-const getCallerIP = (request) => {
-  var ip = request.headers['x-forwarded-for'] ||
-      request.connection.remoteAddress ||
-      request.socket.remoteAddress ||
-      request.connection.socket.remoteAddress;
+const getCallerIP = request => {
+  let ip = request.headers['x-forwarded-for'] ||
+    request.connection.remoteAddress ||
+    request.socket.remoteAddress ||
+    request.connection.socket.remoteAddress;
   ip = ip.split(',')[0];
   ip = ip.split(':').slice(-1); //in case the ip returned in a format: "::ffff:146.xxx.xxx.xxx"
-  return ip;
+  return ip[0];
+};
+
+// Parse caller Host from the request
+const getCallerHost = request => {
+  return request.headers['x-forwarded-host'] ||
+  request.headers.host;
+};
+
+// Extract response message and code from the response object
+const parseResponseCode = (response, data) => {
+  const code = `${response.statusCode} ${response.statusMessage}`;
+  let message;
+
+  if (response.exception) {
+    message = typeof data === 'object' ? JSON.stringify(data) : data;
+  } else {
+    message = 'OK';
+  }
+
+  return {
+    message,
+    code
+  };
+};
+
+// Create properties mapping for index
+const createMapping = async (client, mapping) => {
+  return await client.indices.create(
+    mapping,
+    {
+      ignore: [400]
+    });
 };
 
 // Send exception event to the elasticsearch
-module.exports.indexException = (client, request, error) => setImmediate(async () => {
+module.exports.indexException = (client, args) => setImmediate(async () => {
+  const [request, response, data] = args;
+
   try {
-    await client.index({
+    const { message, code } = parseResponseCode(response, data);
+
+    await createMapping(
+      client,
+      {
+        index: 'glider-exceptions',
+        body: {
+          mappings: {
+            properties: {
+              method: { type: 'keyword' },
+              query: { type: 'object' },
+              body: { type: 'object' },
+              url: { type: 'text' },
+              host: { type: 'text' },
+              time: { type: 'date' },
+              remote: { type: 'text' },
+              message: { type: 'text' },
+              code: { type: 'text' },
+              traceback: { type: 'text' }
+            }
+          }
+        }
+      }
+    );
+
+    const result = await client.index({
       index: 'glider-exceptions',
       body: {
         method: request.method,
         query: request.query,
         body: request.body,
         url: request.url,
+        host: getCallerHost(request),
         time: new Date().toISOString(),
         remote: getCallerIP(request),
-        message: error.message,
-        traceback: error.stack
+        message,
+        code,
+        traceback: response.exception.stack
       }
     });
+
+    if (result instanceof errors.ResponseError) {
+      console.log('Elastic error: ', result);
+    }
   } catch(e) {
     console.error(e);
   }
 });
 
 // Send common event to the elasticsearch
-module.exports.indexEvent = (client, request, response) => setImmediate(async () => {
+module.exports.indexEvent = (client, args) => setImmediate(async () => {
+  const [request, response, data] = args;
+
   try {
     const didDocument =
       request.verificationResult &&
@@ -59,24 +128,58 @@ module.exports.indexEvent = (client, request, response) => setImmediate(async ()
     }
 
     const endTime = process.hrtime(request.start);
+    const { message, code } = parseResponseCode(response, data);
 
-    await client.index({
+    await createMapping(
+      client,
+      {
+        index: 'glider-events',
+        body: {
+          mappings: {
+            properties: {
+              agent: { type: 'text' },
+              method: { type: 'keyword' },
+              query: { type: 'object' },
+              body: { type: 'object' },
+              url: { type: 'text' },
+              host: { type: 'text' },
+              time: { type: 'date' },
+              remote: { type: 'text' },
+              orgid: { type: 'text' },
+              did: { type: 'object' },
+              elapsed: { type: 'integer' },
+              message: { type: 'text' },
+              code: { type: 'text' }
+            }
+          }
+        }
+      }
+    );
+
+    const result = await client.index({
       index: 'glider-events',
       body: {
-        agent: request.verificationResult ? request.verificationResult.iss : undefined,
+        agent: request.verificationResult ? request.verificationResult.iss : '',
         method: request.method,
-        code: response.statusCode,
-        message: response.statusMessage,
         query: request.query,
         body: request.body,
         url: request.url,
+        host: getCallerHost(request),
         time: new Date().toISOString(),
         remote: getCallerIP(request),
-        orgid: didDocument ? didDocument.id : undefined,
+        orgid: request.verificationResult && request.verificationResult.didResult
+          ? request.verificationResult.didResult.id
+          : '',
         did,
-        elapsed: parseInt((endTime[0] * 1000) + (endTime[1]/1000000))
+        elapsed: parseInt((endTime[0] * 1000) + (endTime[1]/1000000)),
+        message,
+        code
       }
     });
+
+    if (result instanceof errors.ResponseError) {
+      console.log('Elastic error: ', result);
+    }
   } catch (e) {
     console.error(e);
   }
