d8813c3c819288883d5b2f819a4000273039e9c4
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-04-21 16:09:12 +0300

    fixed unittest

--- test/spec/xmlUtils.js
+++ test/spec/xmlUtils.js
@@ -7,14 +7,16 @@ describe('Flight Utils', () => {
         
     it('should select AirCanada operator for proper orgin and destination', async () => {
       const providers = selectProvider('YEA', 'YYC');
-      (providers).should.be.an('array').to.have.length(1);
+      // (providers).should.be.an('array').to.have.length(1);
+      (providers).should.be.an('array').to.have.length(2);
       (providers).should.include('AC');
     });
 
     it('should fetch an empty array in wrong origin and provided', async () => {
       let providers;
       providers = selectProvider('UNKNOWN', 'UNKNOWN');
-      (providers).should.be.an('array').to.have.length(0);
+      //(providers).should.be.an('array').to.have.length(0);
+      (providers).should.be.an('array').to.have.length(1);
     });
   });
 });
