e8e1e5d0bbe052a838042526161b66dc890d24df
Kostiantyn Smyrnov <kostysh@gmail.com>
2020-06-09 23:43:01 +0300

    added order creation status  handler

--- api/v1/orders/createWithOffer.js
+++ api/v1/orders/createWithOffer.js
@@ -13,6 +13,28 @@ const {
 const hotelResolver = require('../../../helpers/resolvers/hotel/orderCreateWithOffer');
 const flightResolver = require('../../../helpers/resolvers/flight/orderCreateWithOffer');
 
+const setOrderStatus = async (offer, orderStatus) => {
+  offer = {
+    ...offer,
+    extraData: {
+      ...(
+        offer.extraData
+          ? {
+            ...offer.extraData,
+            orderStatus
+          }
+          : {
+            orderStatus
+          }
+      )
+    }
+  };
+
+  await offerManager.saveOffer(offer.offerId, {
+    offer
+  });
+};
+
 module.exports = basicDecorator(async (req, res) => {
   const requestBody = req.body;
 
@@ -26,134 +48,157 @@ module.exports = basicDecorator(async (req, res) => {
   // Retrieve the offer
   const storedOffer = await offerManager.getOffer(requestBody.offerId);
 
-  let orderCreationResults;
-  let guarantee;
-  let guaranteeClaim;
-
-  if (requestBody.guaranteeId) {
-    // Get the guarantee
-    guarantee = await getGuarantee(requestBody.guaranteeId, storedOffer);
-  
-    // Claim the guarantee
-    guaranteeClaim = await claimGuaranteeWithCard(requestBody.guaranteeId);
+  if (storedOffer.extraData && storedOffer.extraData.orderStatus === 'CREATING') {
+    throw new GliderError(
+      'Order creation ongoing for this offer',
+      400
+    );
   }
 
-  // Handle an Accomodation offer
-  if (storedOffer instanceof AccommodationOffer) {
+  if (storedOffer.extraData && storedOffer.extraData.orderStatus === 'CREATED') {
+    throw new GliderError(
+      'Order already created for this offer',
+      400
+    );
+  }
 
-    if (!guaranteeClaim) {
-      throw new GliderError(
-        'Claimed guarantee is required',
-        400
+  try {
+    await setOrderStatus(storedOffer, 'CREATING');
+
+    let orderCreationResults;
+    let guarantee;
+    let guaranteeClaim;
+
+    if (requestBody.guaranteeId) {
+      // Get the guarantee
+      guarantee = await getGuarantee(requestBody.guaranteeId, storedOffer);
+    
+      // Claim the guarantee
+      guaranteeClaim = await claimGuaranteeWithCard(requestBody.guaranteeId);
+    }
+
+    // Handle an Accomodation offer
+    if (storedOffer instanceof AccommodationOffer) {
+
+      if (!guaranteeClaim) {
+        throw new GliderError(
+          'Claimed guarantee is required',
+          400
+        );
+      }
+
+      // Resolve this query for an hotel offer
+      orderCreationResults = await hotelResolver(
+        storedOffer,
+        requestBody.passengers,
+        guaranteeClaim.card
       );
     }
 
-    // Resolve this query for an hotel offer
-    orderCreationResults = await hotelResolver(
-      storedOffer,
-      requestBody.passengers,
-      guaranteeClaim.card
-    );
-  }
-
-  // Handle a flight offer
-  else if (storedOffer instanceof FlightOffer) {
-    orderCreationResults = await flightResolver(
-      storedOffer,
-      requestBody,
-      guaranteeClaim
-    );
-  }
-
-  // Handle other types of offer
-  else {
-    throw new GliderError(
-      'Unable to understand the offer type',
-      500
-    );
-  }
-
-  // Change passengers Ids to indernal
-  const passengersIndex = Object.entries(requestBody.passengers)
-    .reduce(
-      (a, v) => {
-        a[`${v[1].type}${v[1].lastnames.join('').toUpperCase()}${v[1].firstnames.join('').toUpperCase()}${v[1].birthdate.split('T')[0]}`] = v[0];
-        return a;
-      },
-      {}
-    );
-
-  if (storedOffer instanceof AccommodationOffer) {
-    delete orderCreationResults.order.success;
-    delete orderCreationResults.order.errors;
-  } else if (storedOffer instanceof FlightOffer) {
-    const changedPassengers =
-      Object.entries(orderCreationResults.order.passengers)
-        .reduce(
-          (a, v) => {
-            const index = `${v[1].type}${v[1].lastnames.join('').toUpperCase()}${v[1].firstnames.join('').toUpperCase()}${v[1].birthdate}`;
-            if (passengersIndex[index]) {
-              a.passengers[passengersIndex[index]] = v[1];
-              a.mapping[v[0]] = passengersIndex[index];
-            }
-            return a;
-          },
-          {
-            passengers: {},
-            mapping: {}
-          }
-        );
-    orderCreationResults.order.passengers = changedPassengers.passengers;
-    
-    // Change passengers Ids in travelDocuments to internal values
-    if (orderCreationResults.travelDocuments) {
-      orderCreationResults.travelDocuments.etickets =
-        orderCreationResults.travelDocuments.etickets.map(
-          tickets => {
-            const changedTickets = {};
-            for (const t in tickets) {
-              changedTickets[t] = changedPassengers.mapping[tickets[t]];
-            }
-            return changedTickets;
-          }
-        );
+    // Handle a flight offer
+    else if (storedOffer instanceof FlightOffer) {
+      orderCreationResults = await flightResolver(
+        storedOffer,
+        requestBody,
+        guaranteeClaim
+      );
     }
 
-    // Change segments Ids to internal values
-    const segmentsIndex = storedOffer.extraData.segments
+    // Handle other types of offer
+    else {
+      throw new GliderError(
+        'Unable to understand the offer type',
+        500
+      );
+    }
+
+    // Change passengers Ids to indernal
+    const passengersIndex = Object.entries(requestBody.passengers)
       .reduce(
         (a, v) => {
-          a[`${v.index}`] = v.id;
+          a[`${v[1].type}${v[1].lastnames.join('').toUpperCase()}${v[1].firstnames.join('').toUpperCase()}${v[1].birthdate.split('T')[0]}`] = v[0];
           return a;
         },
         {}
       );
 
-    orderCreationResults.order.itinerary.segments =
-      Object.entries(orderCreationResults.order.itinerary.segments)
+    if (storedOffer instanceof AccommodationOffer) {
+      delete orderCreationResults.order.success;
+      delete orderCreationResults.order.errors;
+    } else if (storedOffer instanceof FlightOffer) {
+      const changedPassengers =
+        Object.entries(orderCreationResults.order.passengers)
+          .reduce(
+            (a, v) => {
+              const index = `${v[1].type}${v[1].lastnames.join('').toUpperCase()}${v[1].firstnames.join('').toUpperCase()}${v[1].birthdate}`;
+              if (passengersIndex[index]) {
+                a.passengers[passengersIndex[index]] = v[1];
+                a.mapping[v[0]] = passengersIndex[index];
+              }
+              return a;
+            },
+            {
+              passengers: {},
+              mapping: {}
+            }
+          );
+      orderCreationResults.order.passengers = changedPassengers.passengers;
+      
+      // Change passengers Ids in travelDocuments to internal values
+      if (orderCreationResults.travelDocuments) {
+        orderCreationResults.travelDocuments.etickets =
+          orderCreationResults.travelDocuments.etickets.map(
+            tickets => {
+              const changedTickets = {};
+              for (const t in tickets) {
+                changedTickets[t] = changedPassengers.mapping[tickets[t]];
+              }
+              return changedTickets;
+            }
+          );
+      }
+
+      // Change segments Ids to internal values
+      const segmentsIndex = storedOffer.extraData.segments
         .reduce(
           (a, v) => {
-            const index = `${v[1].origin.iataCode}${v[1].destination.iataCode}`;
-            if (segmentsIndex[index]) {
-              a[segmentsIndex[index]] = v[1];
-            }
+            a[`${v.index}`] = v.id;
             return a;
           },
           {}
         );
-  }
-  
-  await ordersManager.saveOrder(
-    orderCreationResults.orderId,
-    {
-      provider: storedOffer.provider,
-      request: requestBody,
-      guarantee: guarantee,
-      guaranteeClaim: guaranteeClaim,
-      order: orderCreationResults,
-      offer: storedOffer
-    }
-  );
 
-  res.status(200).json(orderCreationResults);
+      orderCreationResults.order.itinerary.segments =
+        Object.entries(orderCreationResults.order.itinerary.segments)
+          .reduce(
+            (a, v) => {
+              const index = `${v[1].origin.iataCode}${v[1].destination.iataCode}`;
+              if (segmentsIndex[index]) {
+                a[segmentsIndex[index]] = v[1];
+              }
+              return a;
+            },
+            {}
+          );
+    }
+    
+    await ordersManager.saveOrder(
+      orderCreationResults.orderId,
+      {
+        provider: storedOffer.provider,
+        request: requestBody,
+        guarantee: guarantee,
+        guaranteeClaim: guaranteeClaim,
+        order: orderCreationResults,
+        offer: storedOffer
+      }
+    );
+
+    await setOrderStatus(storedOffer, 'CREATED');
+
+    res.status(200).json(orderCreationResults);
+  } catch (error) {
+    await setOrderStatus(storedOffer, 'UNLOCKED');
+    throw error;
+  }
 });
