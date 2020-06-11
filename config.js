5c4c8dbca3bd59701dadaaa82f083b9cc0608963
Mathieu TAHON <mathieu.tahon@noath.net>
2020-06-11 15:31:04 +0200

    Removed defaulted keys and renamed some secrets

--- config.js
+++ config.js
@@ -104,11 +104,11 @@ const airCanadaConfig = {
       Participant: {
         EnabledSystemParticipant: {
           '@SequenceNumber': getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_SEQUENCE_NUMBER') || '1',
-          Name: getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_NAME'),
+          Name: getConfigKey('AC_USERNAME') || getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_NAME'),
           Category: getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_CATEGORY') || 'DC',
           SystemID: {
             '@Owner': getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_SYSTEM_ID_OWNER') || 'ADS',
-            '@value': getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_SYSTEM_ID'),
+            '@value': getConfigKey('AC_PASSWORD') || getConfigKey('AC_PARTICIPANT_ENABLED_SYSTEM_PARTICIPANT_SYSTEM_ID'),
           }
         }
       }
@@ -131,7 +131,7 @@ module.exports.INFURA_URI = `${getConfigKey('INFURA_ENDPOINT')}/${getConfigKey('
 module.exports.GLIDER_DID = `did:orgid:${getConfigKey('GLIDER_ORGID') || '0x71cd1781a3082f33d2521ac8290c9d4b3b3b116e4e8548a4914b71a1f7201da0'}`;
 module.exports.GLIDER_ADMIN_DID = `did:orgid:${getConfigKey('GLIDER_ORGID') || ''}#${getConfigKey('GLIDER_ADMIN_KEY') || ''}`;
 module.exports.SIMARD_URL = getConfigKey('SIMARD_URL') || `https://${enviroment}.api.simard.io/api/v1`;
-module.exports.JWT = getConfigKey('JWT');
+module.exports.SIMARD_JWT = getConfigKey('SIMARD_JWT') || getConfigKey('JWT');
 module.exports.LIF_MIN_DEPOSIT = getConfigKey('LIF_MIN_DEPOSIT') || '0';
 module.exports.expirationTime = 30 * 60; // 30 min in seconds
 module.exports.expirationLong = 60 * 60 * 24 * 365 * 7; // 7 years in seconds
