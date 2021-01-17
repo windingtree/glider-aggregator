6760b138484a9616773a012f78850e8f0da4be3d
kurt <tomashq@gmail.com>
2021-01-17 13:10:26 +0100

    propagate failure response from provider (amadeus, ac, af) to calling client

--- helpers/providers/flights/ac/ndcClientAC.js
+++ helpers/providers/flights/ac/ndcClientAC.js
@@ -1,6 +1,8 @@
 const { airCanadaConfig } = require('../../../../config');
-const { webserviceDefinition } = require('../../../webservice/webserviceClient');
-const WebserviceClient = require('../../../webservice/webserviceClient').WebserviceClient;
+const { createWebserviceDefinition, WebserviceClient } = require('../../../webservice/webserviceClient');
+const { WebserviceCallException } = require('../../../webservice/webserviceException');
+const { CustomError1, CustomError2, CustomError3 } = require('../../../webservice/errors');
+const GliderError = require('../../../error');
 
 const WEBSERVICES = {
   FLIGHTS_SEARCH: 'FLIGHTS_SEARCH',
@@ -14,11 +16,11 @@ const customHeaders = {
   'X-apiKey': airCanadaConfig.apiKey,
 };
 const WEBSERVICES_CONFIG = [
-  webserviceDefinition(WEBSERVICES.FLIGHTS_SEARCH, `${airCanadaConfig.baseUrl}/AirShopping`, undefined, customHeaders),
-  webserviceDefinition(WEBSERVICES.SEATMAP_RETRIEVE, `${airCanadaConfig.baseUrlPci}/SeatAvailability`, undefined, customHeaders),
-  webserviceDefinition(WEBSERVICES.PRICE_OFFER, `${airCanadaConfig.baseUrl}/OfferPrice`, undefined, customHeaders),
-  webserviceDefinition(WEBSERVICES.CREATE_ORDER, `${airCanadaConfig.baseUrlPci}/OrderCreate`, undefined, customHeaders),
-  webserviceDefinition(WEBSERVICES.ORDER_FULFILL, `${airCanadaConfig.baseUrlPci}/OrderCreate`, undefined, customHeaders),
+  createWebserviceDefinition(WEBSERVICES.FLIGHTS_SEARCH, `${airCanadaConfig.baseUrl}/AirShopping`, undefined, customHeaders),
+  createWebserviceDefinition(WEBSERVICES.SEATMAP_RETRIEVE, `${airCanadaConfig.baseUrlPci}/SeatAvailability`, undefined, customHeaders),
+  createWebserviceDefinition(WEBSERVICES.PRICE_OFFER, `${airCanadaConfig.baseUrl}/OfferPrice`, undefined, customHeaders),
+  createWebserviceDefinition(WEBSERVICES.CREATE_ORDER, `${airCanadaConfig.baseUrlPci}/OrderCreate`, undefined, customHeaders),
+  createWebserviceDefinition(WEBSERVICES.ORDER_FULFILL, `${airCanadaConfig.baseUrlPci}/OrderCreate`, undefined, customHeaders),
 ];
 
 let wbsClient;
@@ -48,6 +50,37 @@ const createOrderRQ = async (request) => {
 
 const fulfillOrderRQ = async (request) => {
   return await getWbsClient().wbsRequest(WEBSERVICES.ORDER_FULFILL, request);
+};
+
+
+const translateWebserviceError = (error) => {
+  console.log('Error=>', error, 'serverResponse null?', error.serverResponse===undefined);
+  if(error instanceof CustomError1)
+    console.log('error is CustomError1', error);
+  if(error instanceof CustomError2)
+    console.log('error is CustomError2', error);
+  if(error instanceof CustomError3)
+    console.log('error is CustomError3', error);
+  if(error instanceof WebserviceCallException)
+    console.log('error is WebserviceCallException', error);
+  if(error instanceof GliderError)
+    console.log('error is GliderError', error);
+  if(error instanceof Error)
+    console.log('error is Error', error);
+
+  if (error instanceof WebserviceCallException){
+    console.log('its WebserviceCallException');
+    return new GliderError(error.message, error.httpStatus);
+  }else if (error instanceof GliderError) {
+    console.log('its GliderError');
+    return error;
+  }else if (error instanceof Error) {
+    console.log('its Error');
+    return error;
+  }else{
+    console.log('unknown error');
+    return new GliderError(`Unknown error: ${error.message}, ${error.code}`);
+  }
 };
 
 module.exports = {
